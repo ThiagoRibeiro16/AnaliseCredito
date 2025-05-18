50d3787ded3afe489f29d304d69127a283801890
Thiago Ribeiro <thiago.escola321@gmail.com>
2025-05-18 17:19:19 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+double emprestimo, renda, valorPar, valorRenda;
+int parcelas;
+
+Console.Write("Qual o valor do empréstimo: ");
+emprestimo = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Quantas parcelas: ");
+parcelas = Convert.ToInt16(Console.ReadLine()!);
+
+Console.Write("Qual sua renda: ");
+renda = Convert.ToDouble(Console.ReadLine()!);
+
+valorPar = emprestimo / Convert.ToDouble(parcelas);
+valorRenda = renda * 0.30;
+
+if (valorRenda >= valorPar)
+{
+
+    Console.WriteLine("Liberado!");
+
+    Console.WriteLine($" valor da parcela: {valorPar}");
+    Console.WriteLine($" valor de 30% salario: {valorRenda}");
+
+
+}
+
+else
+{
+    Console.WriteLine("Negado!");
+
+    Console.WriteLine($" valor da parcela: {valorPar}");
+    Console.WriteLine($" valor de 30% salario: {valorRenda}");     
+
+}
